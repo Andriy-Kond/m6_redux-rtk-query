0fc1b14b3c2c6f7a25708ac5601af360580f2c75
Andrii <andrii.kondratovich@gmail.com>
2024-09-30 14:41:31 +0300

    add: refetch cases

--- src/common/components/Tasks/Tasks.jsx
+++ src/common/components/Tasks/Tasks.jsx
@@ -2,22 +2,50 @@ import { AppBar } from "common/components/AppBar/AppBar";
 import { TaskForm } from "common/components/TaskForm/TaskForm";
 import { TasksList } from "common/components/TaskList/TasksList";
 
-import { useFetchTasksQuery } from "../../../features/tasks/tasksSlice";
+import {
+  useFetchTaskByIdQuery,
+  useFetchTasksQuery,
+} from "../../../features/tasks/tasksSlice";
+import { useState } from "react";
 
 export default function Tasks() {
+  const [taskId, setTaskId] = useState("");
+  console.log("Tasks >> taskId:::", taskId);
   const { error, isFetching } = useFetchTasksQuery();
+
+  useFetchTaskByIdQuery(taskId, {
+    skip: taskId === "",
+    // pollingInterval: 3000,
+    refetchOnMountOrArgChange: true,
+    refetchOnFocus: true,
+    refetchOnReconnect: true,
+  });
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    setTaskId(form.elements.queryId.value);
+    form.reset();
+  };
 
   return (
     <>
       {error && <div>{error}</div>}
-      {isFetching && !error && <div>Loading...</div>}
-      {!isFetching && !error && (
+
+      {!error && (
         <>
           <AppBar />
           <TaskForm />
           <TasksList />
         </>
       )}
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="queryId" />
+        <button type="submit"> set id</button>
+      </form>
+
+      {isFetching && !error && <div>Loading...</div>}
     </>
   );
 }
